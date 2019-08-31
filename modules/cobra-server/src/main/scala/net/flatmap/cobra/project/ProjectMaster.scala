623838ee9da0b95a9573553006dba4fee20c8ddf
Nils Mahlstaedt <mahlstni@uni-bremen.de>
2019-08-31 16:38:47 +0200

    fixed parser and some assorted fixes

--- modules/cobra-server/src/main/scala/net/flatmap/cobra/project/ProjectMaster.scala
+++ modules/cobra-server/src/main/scala/net/flatmap/cobra/project/ProjectMaster.scala
@@ -1,11 +1,14 @@
 package net.flatmap.cobra.project
 
+import java.net.URLDecoder
+import java.nio.charset.Charset
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
 import better.files._
 import fastparse._
 import net.flatmap.cobra._
 
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 class ProjectMaster(mainPID: Long, baseDir: File) extends Actor with ActorLogging {
 
@@ -15,12 +18,16 @@ class ProjectMaster(mainPID: Long, baseDir: File) extends Actor with ActorLoggin
 
   def running(projects: Map[String, ActorRef]): Receive = {
     case msg@InitProject(id, _, _, _) if projects.contains(id) =>
+      println(projects)
+
       projects(id).tell(msg, sender)
 
     case msg@InitProject(id, mode, root, srcs) =>
+      println(projects)
+
       ProjectServer
         .props(mainPID, id, mode, root, srcs)
-        .map(context.actorOf) match {
+        .map(context.actorOf(_, id)) match {
         case Failure(exception) =>
           log.error(s"could not initialize project $id", exception)
         case Success(pRef) =>
@@ -39,36 +46,48 @@ class ProjectMaster(mainPID: Long, baseDir: File) extends Actor with ActorLoggin
 
     case msg@GetSnippet(_, _: PathSource) =>
       context.actorOf(Props(new SourceLoadActor(baseDirStr))).forward(msg)
-    case msg@GetSnippet(reqId, LogicalPath(path)) =>
-      (for{
-        (projectkey, snippetPath) <- extractPathParts(path)
-        actor <- projects.get(projectkey).orElse(projects.get(projectkey.toLowerCase()))
-      } yield {
-        (actor, GetSnippet(reqId, LogicalPath(snippetPath)))
-      }).fold(sender() ! UnkownSnippet(reqId)){
-        case (actor, msg) => actor.forward(msg)
+    case GetSnippet(reqId, LogicalPath(path)) =>
+      val t: Either[String, (ActorRef, GetSnippet)] = ProjectMaster.extractPathParts(path).flatMap{
+        case (projectkey,snippetPath) =>
+          projects.get(projectkey)
+            .orElse(projects.get(projectkey.toLowerCase())) match {
+            case None => Left(s"could not find registred project for key: '$projectkey'")
+            case Some(ref) =>
+              log.debug(s"found projectactor $ref for key $projectkey")
+              Right((ref, GetSnippet(reqId, LogicalPath(snippetPath))))
+          }
       }
 
+      t.fold(
+        err => sender() ! UnkownSnippet(reqId, err),
+        ((a: ActorRef, msg: Any) => a.forward(msg)).tupled
+      )
+
     case e => log.warning(s"received unkown message: $e")
-  }
-
-  private def projectParser[_: P]: P[(String, String)] = {
-    import NoWhitespace._
-
-    val projectKey = P("[" ~ CharsWhileIn(" a-zA-Z0-9").?.! ~ "]")
-    val snippetPath = P(CharsWhile(!_.isWhitespace).!)
-
-    P(Start ~ projectKey ~/ " ".rep(1) ~/ snippetPath ~ End)
-  }
-
-  private def extractPathParts(path: String): Option[(String, String)] = {
-    parse(path, projectParser(_)) match {
-      case Parsed.Success(value, index) => Some(value)
-      case _: Parsed.Failure => None
-    }
   }
 }
 
 object ProjectMaster {
   def props(mainPID: Long, baseDir: File): Props = Props(new ProjectMaster(mainPID, baseDir))
+
+  private def projectParser[_: P]: P[(String, String)] = {
+    import SingleLineWhitespace._
+
+    def projectKey: P[String] = P("[" ~ CharsWhile(_.isLetterOrDigit).! ~ "]")
+    def snippetPath: P[String] = P(CharsWhile(!_.isWhitespace).!)
+
+    P(Start ~ projectKey ~ snippetPath ~ End)
+  }
+
+  def extractPathParts(path: String): Either[String, (String, String)] = {
+//    Try(URLDecoder.decode(path, Charset.forName("utf-8").name()))
+//      .toEither
+//      .left.map(_ => s"could not URL decode input '$path'")
+//      .flatMap{ decoded =>
+        parse(path, projectParser(_)) match {
+          case Parsed.Success(value, _) => Right(value)
+          case Parsed.Failure(label, index, extra) => Left(s"could not parse path $label at col $index")
+        }
+      //}
+  }
 }
