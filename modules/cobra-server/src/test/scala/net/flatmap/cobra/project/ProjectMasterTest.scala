35c730e18380b40211921b7dc53b002120c2308f
Nils <mahlstni@uni-bremen.de>
2019-09-10 12:53:21 +0200

    updated and extended tests

--- modules/cobra-server/src/test/scala/net/flatmap/cobra/project/ProjectMasterTest.scala
+++ modules/cobra-server/src/test/scala/net/flatmap/cobra/project/ProjectMasterTest.scala
@@ -1,86 +1,121 @@
 package net.flatmap.cobra.project
 
-import net.flatmap.cobra.project.ProjectMaster.{Path, ProjectAssociation, TypeBound}
+import fastparse.Parsed.{Failure, Success}
+import net.flatmap.cobra.paths.{Path, PathParser, ProjectAssociation, TypeBound}
 import org.eclipse.lsp4j.SymbolKind
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, Inside, Matchers}
+import fastparse._
+import net.flatmap.cobra.paths.PathParser._
 
-class ProjectMasterTest extends FlatSpec with Matchers {
+class ProjectMasterTest extends FlatSpec with Matchers with Inside with PathParser {
 
   "The Path Parser" should "recognize everything enclosed within square brackets as keys" in {
-    ProjectMaster.extractPathParts("[p:key] foobar") shouldBe Right(Path("foobar", ProjectAssociation("key")))
-    ProjectMaster.extractPathParts("[p:key] foobar") shouldBe Right(Path("foobar.bar.baz", ProjectAssociation("key")))
-    ProjectMaster.extractPathParts("[t:function] foobar") shouldBe Right(Path("foobar", TypeBound(SymbolKind.Function)))
-    ProjectMaster.extractPathParts("[p:key] [t:Function] foobar") shouldBe Right(Path("foobar", ProjectAssociation("key"), TypeBound(SymbolKind.Function)))
-    ProjectMaster.extractPathParts("[t:Function] [p:key] foobar") shouldBe Right(Path("foobar", ProjectAssociation("key"), TypeBound(SymbolKind.Function)))
-    ProjectMaster.extractPathParts("[t:Function][p:key] foobar") shouldBe Right(Path("foobar", ProjectAssociation("key"), TypeBound(SymbolKind.Function)))
+
+    extractPathParts("[p:key] foobar") shouldBe Right(Path("foobar", ProjectAssociation("key")))
+    extractPathParts("[p:key] foobar") shouldBe Right(Path("foobar.bar.baz", ProjectAssociation("key")))
+    extractPathParts("[t:function] foobar") shouldBe Right(Path("foobar", TypeBound(SymbolKind.Function)))
+    extractPathParts("[p:key] [t:Function] foobar") shouldBe Right(Path("foobar", ProjectAssociation("key"), TypeBound(SymbolKind.Function)))
+    extractPathParts("[t:Function] [p:key] foobar") shouldBe Right(Path("foobar", ProjectAssociation("key"), TypeBound(SymbolKind.Function)))
+    extractPathParts("[t:Function][p:key] foobar") shouldBe Right(Path("foobar", ProjectAssociation("key"), TypeBound(SymbolKind.Function)))
   }
 
   it should "recognize the path as everything after the key when separated by a space" in {
-    ProjectMaster.extractPathParts("[p:path] path") shouldBe Right(Path("path", ProjectAssociation("path")))
-    ProjectMaster.extractPathParts("[p:key]path") shouldBe Right(Path("path", ProjectAssociation("key")))
-    ProjectMaster.extractPathParts("[p:key] some.path") shouldBe Right(Path("some.path", ProjectAssociation("key")))
+    extractPathParts("[p:path] path") shouldBe Right(Path("path", ProjectAssociation("path")))
+    extractPathParts("[p:key]path") shouldBe Right(Path("path", ProjectAssociation("key")))
+    extractPathParts("[p:key] some.path") shouldBe Right(Path("some.path", ProjectAssociation("key")))
   }
 
   it should "not allow empty keys" in {
-    ProjectMaster.extractPathParts("[] foobar") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[p:] foobar") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[t:] foobar") shouldBe a[Left[_, _]]
+    extractPathParts("[] foo") shouldBe Right(Path("foo"))
+    extractPathParts("[][] foo") shouldBe Right(Path("foo"))
+    extractPathParts("[] [] foo") shouldBe Right(Path("foo"))
+    extractPathParts("[p:] foo") shouldBe Right(Path("foo"))
+    extractPathParts("[t:] foo") shouldBe Right(Path("foo"))
+    extractPathParts("[p:][t:] foo") shouldBe Right(Path("foo"))
+    extractPathParts("[t:][p:] foo") shouldBe Right(Path("foo"))
+  }
+
+  it should "accept correctly formatted type bounds" in {
+    inside(parse("file", typeBound(_))) {
+      case Success(TypeBound(t), _) => t shouldBe SymbolKind.File
+    }
+
+    inside(parse("File", typeBound(_))) {
+      case Success(TypeBound(t), _) => t shouldBe SymbolKind.File
+    }
+
+    inside(parse("FILE", typeBound(_))) {
+      case Success(TypeBound(t), _) => t shouldBe SymbolKind.File
+    }
+
+    inside(parse("EnumMember", typeBound(_))) {
+      case Success(TypeBound(t), _) => t shouldBe SymbolKind.EnumMember
+    }
+
+    inside(parse("enummember", typeBound(_))) {
+      case Success(TypeBound(t), _) => t shouldBe SymbolKind.EnumMember
+    }
+
+    parse("foobar", typeBound(_)) shouldBe a[Failure]
+    parse("EnumMmbr", typeBound(_)) shouldBe a[Failure]
   }
 
   it should "allow whitespace between defining parts in keys" in {
-    ProjectMaster.extractPathParts("[p:path] path") shouldBe Right(Path("path", ProjectAssociation("path")))
-    ProjectMaster.extractPathParts("[  p    :path] path") shouldBe Right(Path("path", ProjectAssociation("path")))
-    ProjectMaster.extractPathParts("[  p :path] path") shouldBe Right(Path("path", ProjectAssociation("path")))
+    extractPathParts("[p:path] path") shouldBe Right(Path("path", ProjectAssociation("path")))
+    extractPathParts("[  p    :path] path") shouldBe Right(Path("path", ProjectAssociation("path")))
+    extractPathParts("[  p :path] path") shouldBe Right(Path("path", ProjectAssociation("path")))
   }
 
   it should "not allow duplicate keys" in {
-    ProjectMaster.extractPathParts("[p:key][p:key] foobar") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[t:key][t:key] foobar") shouldBe a[Left[_, _]]
+    extractPathParts("[p:key] path") shouldBe Right(Path("path", ProjectAssociation("key")))
+    extractPathParts("[f:file] path") shouldBe Right(Path("path", TypeBound(SymbolKind.File)))
+    extractPathParts("[p:key][p:key] foobar") shouldBe a[Left[_, _]]
+    extractPathParts("[t:File][t:file] foobar") shouldBe a[Left[_, _]]
   }
 
   it should "only allow type bounds defined in the LSP spec" in {
-    ProjectMaster.extractPathParts("[t:File] path") shouldBe Right(Path("path", TypeBound(SymbolKind.File)))
-    ProjectMaster.extractPathParts("[t:file] path") shouldBe Right(Path("path", TypeBound(SymbolKind.File)))
-    ProjectMaster.extractPathParts("[t:FILE] path") shouldBe Right(Path("path", TypeBound(SymbolKind.File)))
-    ProjectMaster.extractPathParts("[t:EnumMember] path") shouldBe Right(Path("path", TypeBound(SymbolKind.EnumMember)))
+    extractPathParts("[t:File] path") shouldBe Right(Path("path", TypeBound(SymbolKind.File)))
+    extractPathParts("[t:file] path") shouldBe Right(Path("path", TypeBound(SymbolKind.File)))
+    extractPathParts("[t:FILE] path") shouldBe Right(Path("path", TypeBound(SymbolKind.File)))
+    extractPathParts("[t:EnumMember] path") shouldBe Right(Path("path", TypeBound(SymbolKind.EnumMember)))
 
-    ProjectMaster.extractPathParts("[t:Enummember] path") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[t:enumMember] path") shouldBe a[Left[_, _]]
+    extractPathParts("[t:Enummember] path") shouldBe a[Left[_, _]]
+    extractPathParts("[t:enumMember] path") shouldBe a[Left[_, _]]
   }
 
   it should "not allow keys without tag" in {
-    ProjectMaster.extractPathParts("[key] foobar") shouldBe a[Left[_, _]]
+    extractPathParts("[key] foobar") shouldBe a[Left[_, _]]
   }
 
   it should "not allow keys without assigned tag" in {
-    ProjectMaster.extractPathParts("[key:key] foobar") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[k:key] foobar") shouldBe a[Left[_, _]]
+    extractPathParts("[key:key] foobar") shouldBe a[Left[_, _]]
+    extractPathParts("[k:key] foobar") shouldBe a[Left[_, _]]
   }
 
   it should "not allow empty paths" in {
-    ProjectMaster.extractPathParts("[p:key] ") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[t:key] ") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[p:key]") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[t:key]") shouldBe a[Left[_, _]]
+    extractPathParts("[p:key] ") shouldBe a[Left[_, _]]
+    extractPathParts("[t:key] ") shouldBe a[Left[_, _]]
+    extractPathParts("[p:key]") shouldBe a[Left[_, _]]
+    extractPathParts("[t:key]") shouldBe a[Left[_, _]]
   }
 
   it should "only allow alpha-nums in project key" in {
-    ProjectMaster.extractPathParts("[p:käey908dajkjk3] foo") shouldBe Right(Path("foo",ProjectAssociation("käey908dajkjk3")))
-    ProjectMaster.extractPathParts("[p: ] foo") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[p:-] foo") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[p:_] foo") shouldBe a[Left[_, _]]
+    extractPathParts("[p:käey908dajkjk3] foo") shouldBe Right(Path("foo",ProjectAssociation("käey908dajkjk3")))
+    extractPathParts("[p: ] foo") shouldBe a[Left[_, _]]
+    extractPathParts("[p:-] foo") shouldBe a[Left[_, _]]
+    extractPathParts("[p:_] foo") shouldBe a[Left[_, _]]
   }
 
   it should "allow everything except whitespace in path" in {
-    ProjectMaster.extractPathParts("[p:key] foo") shouldBe Right(Path("foo", ProjectAssociation("key")))
-    ProjectMaster.extractPathParts("[p:key] foo.bar#baz") shouldBe Right(Path("foo.bar#baz", ProjectAssociation("key")))
-    ProjectMaster.extractPathParts("[p:key] foo-bar") shouldBe Right(Path("foo-bar", ProjectAssociation("key")))
-    ProjectMaster.extractPathParts("[p:key] foo_bar") shouldBe Right(Path("foo_bar", ProjectAssociation("key")))
-    ProjectMaster.extractPathParts("[p:key] foo?ß0bär") shouldBe Right(Path("foo?ß0bär", ProjectAssociation("key")))
+    extractPathParts("[p:key] foo") shouldBe Right(Path("foo", ProjectAssociation("key")))
+    extractPathParts("[p:key] foo.bar#baz") shouldBe Right(Path("foo.bar#baz", ProjectAssociation("key")))
+    extractPathParts("[p:key] foo-bar") shouldBe Right(Path("foo-bar", ProjectAssociation("key")))
+    extractPathParts("[p:key] foo_bar") shouldBe Right(Path("foo_bar", ProjectAssociation("key")))
+    extractPathParts("[p:key] foo?ß0bär") shouldBe Right(Path("foo?ß0bär", ProjectAssociation("key")))
 
-    ProjectMaster.extractPathParts("[p:key] foo bar baz") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[p:key] foo\tbarbaz") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[p:key] foo\nbarbaz") shouldBe a[Left[_, _]]
-    ProjectMaster.extractPathParts("[p:key] foo\rbarbaz") shouldBe a[Left[_, _]]
+    extractPathParts("[p:key] foo bar baz") shouldBe a[Left[_, _]]
+    extractPathParts("[p:key] foo\tbarbaz") shouldBe a[Left[_, _]]
+    extractPathParts("[p:key] foo\nbarbaz") shouldBe a[Left[_, _]]
+    extractPathParts("[p:key] foo\rbarbaz") shouldBe a[Left[_, _]]
   }
 }
