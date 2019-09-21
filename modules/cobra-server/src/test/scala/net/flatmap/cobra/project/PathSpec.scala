16558f25e7fdfcbebd21cf515f33da688486fe2c
Nils <mahlstni@uni-bremen.de>
2019-09-21 13:38:56 +0200

    added test for absoluteness of paths

---
+++ modules/cobra-server/src/test/scala/net/flatmap/cobra/project/PathSpec.scala
@@ -0,0 +1,11 @@
+package net.flatmap.cobra.project
+
+import net.flatmap.cobra.paths.Path
+import org.scalatest.{FlatSpec, Matchers}
+
+class PathSpec extends FlatSpec with Matchers {
+  "Paths" should "be absolute, if they start with a single '/'" in {
+    Path("/absolute").isAbsolute shouldBe true
+    Path("relative").isAbsolute shouldBe false
+  }
+}
