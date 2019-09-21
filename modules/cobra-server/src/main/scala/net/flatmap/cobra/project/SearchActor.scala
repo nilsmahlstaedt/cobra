cc29e147d6240742dd6b02c9d750b7b33122bbcf
Nils <mahlstni@uni-bremen.de>
2019-09-21 14:38:27 +0200

    added code comments to the search actor

--- modules/cobra-server/src/main/scala/net/flatmap/cobra/project/SearchActor.scala
+++ modules/cobra-server/src/main/scala/net/flatmap/cobra/project/SearchActor.scala
@@ -3,7 +3,7 @@ package net.flatmap.cobra.project
 import akka.actor.{Actor, ActorLogging, ActorRef, ReceiveTimeout}
 import net.flatmap.cobra.{AmbiguousDefinition, Mode, ResolvedSnippet, ServerMessage, Snippet, SnippetDef, SnippetResolver, UnkownSnippet}
 import net.flatmap.cobra.paths.Path
-import net.flatmap.cobra.project.SearchActor.SnippetResponse
+import net.flatmap.cobra.project.SearchActor.{RequestSnippets, SnippetResponse}
 
 import scala.concurrent.duration._
 import scala.util.Properties
@@ -15,16 +15,40 @@ class SearchActor(projects: Map[String, ActorRef], replyTo: ActorRef, reqId: Str
 
   override def preStart(): Unit = {
     super.preStart()
+    // stop self if not receiving an answer for a second
     context.setReceiveTimeout(1.second)
+
+    val toSearch = projectsToSearch(projects, path)
+
+    //query actors
+    toSearch.foreach(_ ! RequestSnippets(path))
+
+    // start collecting responses
+    context.become(collecting(toSearch.toSet, Nil))
   }
 
-  def projectsToSearch(ps: Map[String, ActorRef], p: Path): List[ActorRef] = {
+  /**
+   * returns the projectserver actors to search
+   *
+   * will try to return only the explicitly stated project if done,
+   * otherwise returns all projects if none is specified
+   *
+   * @param ps projects maps (projectID -> server actor ref)
+   * @param p path to search
+   * @return actors to query for results
+   */
+  private def projectsToSearch(ps: Map[String, ActorRef], p: Path): List[ActorRef] = {
     p.projectAssociation()
       .fold(projects.values.toList) { pa =>
         ps.get(pa.project).toList
       }
   }
 
+  /**
+   * answer collecting state of actor
+   * @param waitingFor queried refs who haven't answered yet
+   * @param answers answers received
+   */
   def collecting(waitingFor: Set[ActorRef], answers: List[SnippetResponse]): Receive = {
     case msg: SnippetResponse =>
       val nextWaitingFor = waitingFor - sender()
@@ -43,10 +67,20 @@ class SearchActor(projects: Map[String, ActorRef], replyTo: ActorRef, reqId: Str
       context.stop(self)
   }
 
+  /**
+   * default behaviour
+   */
   def receive = {
     case ReceiveTimeout => context.stop(self)
   }
 
+  /**
+   * analyses collected responses and formulates response
+   * formulated response answers the search encapsulated in this actor
+   * @param resps collected responses
+   * @param missingAnswers number of missing answers
+   * @return a message that
+   */
   def produceResponse(resps: List[SnippetResponse], missingAnswers: Int): ServerMessage = {
     case class SearchResult(project: String, mode:Mode, kind: String, snippet: Snippet){
       def toSnippetDef: SnippetDef =
