650ce9ceb496f412297234f249380dd574927308
Nils <mahlstni@uni-bremen.de>
2019-09-21 14:24:19 +0200

    Made SnippetDict a trait with implicit function on lists of snippets - part 2

--- modules/cobra-server/src/main/scala/net/flatmap/cobra/project/SnippetSearch.scala
+++ modules/cobra-server/src/main/scala/net/flatmap/cobra/project/SnippetSearch.scala
@@ -5,25 +5,25 @@ import net.flatmap.cobra.paths.{Path, ProjectAssociation, TypeBound}
 import org.eclipse.lsp4j.SymbolKind
 
 /**
- * contains all snippets for one project
- * @param snippets snippets to include in the dictionary
+ * makes list of snippets searchable
  */
-class SnippetDictionary(snippets: List[Snippet]) {
-
-  def find(path:Path): List[Snippet] = {
-    val typed: List[Snippet] = snippets.filter(s => path.typeBound().forall(bound => s.kind == bound.typ))
-    if(path.isAbsolute){
-      // path is absolute match front to end
-      typed.filter(s => {
-        val snippetPath = Path.buildPathString(s)
-        path.path.equalsIgnoreCase(snippetPath)
-      })
-    }else{
-      // path is just a partial path, match from end
-      typed.filter(s => {
-        val snippetPath = Path.buildPathString(s).toLowerCase
-        snippetPath.toLowerCase.endsWith(path.path)
-      })
+trait SnippetSearch{
+  implicit class SnippetDictionary(snippets: List[Snippet]) {
+    def findSnippets(path:Path): List[Snippet] = {
+      val typed: List[Snippet] = snippets.filter(s => path.typeBound().forall(bound => s.kind == bound.typ))
+      if(path.isAbsolute){
+        // path is absolute match front to end
+        typed.filter(s => {
+          val snippetPath = Path.buildPathString(s)
+          path.path.equalsIgnoreCase(snippetPath)
+        })
+      }else{
+        // path is just a partial path, match from end
+        typed.filter(s => {
+          val snippetPath = Path.buildPathString(s).toLowerCase
+          snippetPath.toLowerCase.endsWith(path.path)
+        })
+      }
     }
   }
 }
