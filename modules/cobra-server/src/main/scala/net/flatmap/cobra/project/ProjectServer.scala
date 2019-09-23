1e785c68f65a098caeeb0d43e29a79eb8c06271c
Nils <mahlstni@uni-bremen.de>
2019-09-23 21:36:59 +0200

    added project file watching in projectserveractor

--- modules/cobra-server/src/main/scala/net/flatmap/cobra/project/ProjectServer.scala
+++ modules/cobra-server/src/main/scala/net/flatmap/cobra/project/ProjectServer.scala
@@ -1,14 +1,18 @@
 package net.flatmap.cobra.project
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.{Path, Paths, StandardWatchEventKinds, WatchService}
 
-import akka.actor.{Actor, ActorLogging, Props}
-import net.flatmap.cobra.languageserver.{LSInteraction, LSLauncher}
-import net.flatmap.cobra.util.FileUtils
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import net.flatmap.cobra._
+import net.flatmap.cobra.languageserver.{LSInteraction, LSLauncher}
 import net.flatmap.cobra.project.SearchActor.{RequestSnippets, SnippetResponse}
+import net.flatmap.cobra.util.FileUtils
+import org.eclipse.lsp4j.services.LanguageServer
+import better.files._
+import FileWatcher._
+import net.flatmap.cobra.project.ProjectServer.ProjectFileUpdate
 
-import scala.util.{Properties, Success, Try}
+import scala.util.{Success, Try}
 
 class ProjectServer(projectId: String, pid: Long, language: Language, mode:Mode, rootPath: Path, srcRoots: List[Path]) extends Actor with ActorLogging with SnippetSearch {
 
@@ -21,18 +25,32 @@ class ProjectServer(projectId: String, pid: Long, language: Language, mode:Mode,
     }
   }
 
+  /**
+   * holds language server to be exited when actor dies
+   */
+  private var languageServerPlaceholder: Option[LanguageServer] = None
+
   override def preStart(): Unit = {
     super.preStart()
 
+    implicit val system: ActorSystem = context.system
     log.debug(s"Project Server $projectId initializing!")
 
+    log.debug("start language server")
     (for {
-      ls <- LSLauncher.launch(language, rootPath, Some(pid))
+      ls: LanguageServer <- LSLauncher.launch(language, rootPath, Some(pid))
     } yield {
+      languageServerPlaceholder = Some(ls)
+
+      log.debug("finding project files")
       val projectFiles = FileUtils.findProjectFiles(language, srcRoots.orIfEmpty(rootPath)).getOrElse(Nil).distinct
+      log.debug("analyzing project files")
       val snippets = LSInteraction.analyzeProjectFiles(ls, projectFiles)
 
-      ls.exit()
+      log.debug("creating file watches")
+      snippets.flatMap(s => Option(s.source.normalize().getParent).map((_, s.source)).toList).foreach((watchFile _).tupled)
+
+      //ls.exit()
 
       log.info(s"ProjectServer for project $projectId is initialized")
 
@@ -53,7 +71,7 @@ class ProjectServer(projectId: String, pid: Long, language: Language, mode:Mode,
 //          acc + (p -> s)
 //      })
 
-      context.become(running(snippets))
+      context.become(running(ls, snippets))
     }) recover {
       case e: Throwable =>
         log.error(e, s"could not start project actor for project '$projectId'")
@@ -63,13 +81,55 @@ class ProjectServer(projectId: String, pid: Long, language: Language, mode:Mode,
     log.debug(s"Project Server $projectId initialized")
   }
 
+  override def postStop(): Unit = {
+    super.postStop()
+    languageServerPlaceholder.foreach(_.exit())
+  }
+
+  /**
+   * creates filewatch for snippetPath in directory dir
+   *
+   * sends watch events to self
+   *
+   * @param dir directory path to watch for changes
+   * @param snippetPath path to watch out for in dir
+   */
+  private def watchFile(dir: Path, snippetPath: Path) (implicit sys: ActorSystem): Unit = {
+    context.actorOf(File(dir).watcherProps(recursive = false)) ! when(
+      StandardWatchEventKinds.ENTRY_CREATE,
+      StandardWatchEventKinds.ENTRY_DELETE,
+      StandardWatchEventKinds.ENTRY_MODIFY) {
+      case (event, file) if file.path.equals(snippetPath) =>
+        log.debug(s"${context.self.path.name} got file update for existing file $file")
+        self ! ProjectFileUpdate(event, file)
+      case (event, file) if mode.fileendings.contains(file.extension.getOrElse("")) =>
+        log.debug(s"${context.self.path.name} got file update for new file $file")
+        self ! ProjectFileUpdate(event, file)
+      case (event, file) =>
+        log.debug(s"IGNORED - file update $event for $file")
+    }
+  }
+
   override def receive: Receive = {
       case e => log.warning(s"received message before initializing! msg:$e")
   }
 
-  def running(dict: List[Snippet]): Receive = {
+  def running(ls: LanguageServer, dict: List[Snippet]): Receive = {
     case InitProject(`projectId`, _, _, _) => sender() ! ProjectInitialized(projectId)
     case RequestSnippets(path) => sender() ! SnippetResponse(projectId, mode, dict.findSnippets(path))
+
+      // update dict when project files change
+    case ProjectFileUpdate(StandardWatchEventKinds.ENTRY_CREATE, file) =>
+      LSInteraction.analyzeFile(ls)(file.path).foreach(newSnippets => context.become(running(ls, dict ++ newSnippets)))
+    case ProjectFileUpdate(StandardWatchEventKinds.ENTRY_DELETE, file) =>
+      val pathToRemove = file.path
+      context.become(running(ls, dict.filterNot(_.source.equals(pathToRemove))))
+    case ProjectFileUpdate(StandardWatchEventKinds.ENTRY_MODIFY, file) =>
+      val pathOfUpdate = file.path
+      val oldRemoved = dict.filterNot(_.source.equals(pathOfUpdate))
+      val newSnippets = LSInteraction.analyzeFile(ls)(pathOfUpdate).toOption.toList.flatten
+      context.become(running(ls, oldRemoved ++ newSnippets))
+
     case e => log.error(s"received unknown message: $e")
   }
 
@@ -96,6 +156,9 @@ class ProjectServer(projectId: String, pid: Long, language: Language, mode:Mode,
 }
 
 object ProjectServer {
+
+  case class ProjectFileUpdate(event: Event, file: File)
+
   /**
    * generate props for a new project server
    * @param pid cobra main thread pid (watched by LS for auto exit if dead)
