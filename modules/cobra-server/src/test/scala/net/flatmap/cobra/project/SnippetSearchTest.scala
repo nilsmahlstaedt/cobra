ed06d9d6e15a4034d0924817c62e38ab104d0486
Nils Mahlstaedt <mahlstni@uni-bremen.de>
2019-10-03 11:43:04 +0200

    first tests for search algorithm

---
+++ modules/cobra-server/src/test/scala/net/flatmap/cobra/project/SnippetSearchTest.scala
@@ -0,0 +1,62 @@
+package net.flatmap.cobra.project
+
+import java.nio.file.Paths
+
+import net.flatmap.cobra.Snippet
+import net.flatmap.cobra.paths.{Path, TypeBound}
+import org.eclipse.lsp4j.SymbolKind
+import org.scalatest.{FlatSpec, FunSuite, Matchers}
+
+class SnippetSearchTest extends FlatSpec with Matchers with SnippetSearch {
+  val snippets: List[Snippet] = List(
+    Snippet("myclass", Some("some/package/"), Paths.get("testfile"), 0, 0, SymbolKind.Class),
+    Snippet("myclass", Some("some/package/"), Paths.get("testfile"), 0, 0, SymbolKind.Object),
+    Snippet("myfunc", Some("some/package/myclass/"), Paths.get("testfile"), 0, 0, SymbolKind.Function),
+  )
+
+  "SnippetSearch" should "return all matches for an incomplete absolute path" in {
+    val p = Path("/some/package/")
+    val res = snippets.findSnippets(p)
+    res.length shouldBe 3
+    res shouldBe snippets
+  }
+
+  it should "return all matches for an incomplete relative path" in {
+    val res = snippets.findSnippets(Path("package"))
+    res.length shouldBe 2
+    res shouldBe snippets.drop(1)
+  }
+
+  it should "find a complete path" in {
+    val res = snippets.findSnippets(Path("/some/package/myclass"))
+    res.length shouldBe 1
+    res.head shouldBe snippets(3)
+
+    val res2 = snippets.findSnippets(Path("/no/in/some/package"))
+    res2 shouldBe Nil
+  }
+
+  it should "find snippets with relativ paths" in {
+    snippets.findSnippets(Path("package/myclass")) shouldBe
+      snippets.findSnippets(Path("myclass"))
+  }
+
+  it should "treat a complete path the same no matter if it is absolute or not" in {
+    snippets.findSnippets(Path("some/package/myclass")) shouldBe
+      snippets.findSnippets(Path("/some/package/myclass"))
+
+    snippets.findSnippets(Path("some/package")) shouldBe
+      snippets.findSnippets(Path("/some/package"))
+  }
+
+  it should "respect type bounds" in {
+    val res = snippets.findSnippets(Path("some/package", TypeBound(SymbolKind.Class)))
+    res.length shouldBe 1
+    res.head shouldBe snippets(1)
+
+    val res2 = snippets.findSnippets(Path("some/package", TypeBound(SymbolKind.Object)))
+    res2.length shouldBe 1
+    res2.head shouldBe snippets(2)
+  }
+
+}
