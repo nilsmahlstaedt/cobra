5ea7b0bb2ea40cc06eeb4171ad26c5bed418c132
Nils Mahlstaedt <mahlstni@uni-bremen.de>
2020-01-14 11:00:00 +0100

    rebuild DocumentSymbold Conversion on Visitor Pattern

--- modules/cobra-lsbinding/src/main/scala/net/flatmap/cobra/Snippet.scala
+++ modules/cobra-lsbinding/src/main/scala/net/flatmap/cobra/Snippet.scala
@@ -3,26 +3,36 @@ package net.flatmap.cobra
 import java.io.File
 import java.net.URI
 import java.nio.file.{Path, Paths}
-
 import scala.jdk.CollectionConverters._
 import org.eclipse.lsp4j.{DocumentSymbol, SymbolInformation, SymbolKind}
-
-import scala.annotation.tailrec
 
 case class Snippet(name: String, parent: Option[String], source: Path, startLine: Int, endLine: Int, kind: SymbolKind)
 
 object Snippet {
   def apply(source: Path, e: Either[SymbolInformation, DocumentSymbol]): List[Snippet]  = e match {
-  case Left(s) => Snippet(
-    name = s.getName,
-    parent = Option(s.getContainerName),
-    source = Paths.get(URI.create(s.getLocation.getUri).getPath),
-    startLine = s.getLocation.getRange.getStart.getLine,
-    endLine = s.getLocation.getRange.getEnd.getLine,
-    kind = s.getKind
-  ) :: Nil
-  case Right(d) => buildSnippets(d, "", source)//so far i haven't encountered a Document Symbol
-      //TODO implement behaviour
+    case Left(s) => Snippet(
+      name = s.getName,
+      parent = Option(s.getContainerName),
+      source = Paths.get(URI.create(s.getLocation.getUri).getPath),
+      startLine = s.getLocation.getRange.getStart.getLine,
+      endLine = s.getLocation.getRange.getEnd.getLine,
+      kind = s.getKind
+    ) :: Nil
+    case Right(d) => d.toSnippets("", source)//so far i haven't encountered a Document Symbol
+  }
+
+  /**
+   * Visitor for DocumentSymbols
+   * offers visitor pattern based conversion to a flattened list of snippets
+   * @param ds Document Symbold
+   */
+  private implicit class DocumentSymbolVisitor(ds: DocumentSymbol) {
+    def toSnippets(parentPath: String, source: Path): List[Snippet] = {
+      val snippet: Snippet = dsToS(ds, parentPath, source)
+      val children: List[DocumentSymbol] = ds.getChildren.asScala.toList
+
+      snippet :: children.flatMap(_.toSnippets(parentPath+"/"+snippet.name, source))
+    }
   }
 
   private def dsToS(ds: DocumentSymbol, path: String, source: Path): Snippet = {
@@ -34,12 +44,5 @@ object Snippet {
       ds.getRange.getEnd.getLine,
       ds.getKind
     )
-  }
-
-  private def buildSnippets(symbol: DocumentSymbol, path: String = "", source: Path): List[Snippet] = {
-    val snippet = dsToS(symbol, path, source)
-    val children = symbol.getChildren.asScala.toList
-
-    snippet :: children.flatMap(buildSnippets(_, path+"/"+snippet.name, source))
   }
 }
